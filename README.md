bbefd1cf0e992688b1c5ce99ff7e55b4812fd2b9
Ed <kvl.olg@gmail.com>
2019-03-03 10:52:25 +0100

    ci: Add coverage reports (#3)

--- README.md
+++ README.md
@@ -2,9 +2,6 @@ node.js grammar / language tool
 =====================
 
 TODO:
-- add linter && prettier
-- Setup automated build with Circle
-- Analyze code-coverage with Codecov or whatever
 - Add badges to your readme with Shields
 - Add more description
 - Update Trembita with plugin info
--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   testEnvironment: 'node',
   collectCoverageFrom: ['lib/**/*.js', '!**/node_modules/**', '!**/__mocks__/**'],
   testPathIgnorePatterns: ['<rootDir>/__tests__/__responses__/'],
-  coverageReporters: ['text-summary', 'json' /*, "html"*/],
+  // coverageReporters: ['text-summary', 'json' /*, "html"*/],
   collectCoverage: true,
   coverageDirectory: 'coverage/',
 };
